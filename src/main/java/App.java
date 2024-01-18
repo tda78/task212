b99958e3d6502bd79cff882499e4dbc6312d2350
tda78 <tda78@mail.ru>
2024-01-18 19:57:09 +0700

    ready

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean1);
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2 == cat1);
+
+
     }
 }
